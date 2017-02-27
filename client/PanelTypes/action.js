d2976587c129de868e6f308627e4d6e3aabfc7d7
Sanji <sanji2294@gmail.com>
2017-02-27 04:10:58 -0500

    new animations added

--- client/PanelTypes/action.js
+++ client/PanelTypes/action.js
@@ -9,8 +9,27 @@ function Action( action ,  i, agents, name)
 	var img =loadAnimation("assets/closeUp_Frame.png") ;
 	this.frame = createSprite(i*250,100,20,20);
     this.frame.addAnimation("frame",img);
-   
-	this.animation = loadAnimation("assets/action/"+this.action.name+"/"+this.action.name+"_0001.png","assets/action/"+this.action.name+"/"+this.action.name+"_0003.png");
+     if(this.action.name === "propel" || this.action.name === "moveobject" || this.action.name === "movepossesion")
+     {
+     	this.animation = loadAnimation("assets/action/"+"propel"+"/"+"propel"+"_0001.png","assets/action/"+"propel"+"/"+"propel"+"_0007.png");
+     }
+     if(this.action.name === "movebodypart" )
+     {
+     	this.animation = loadAnimation("assets/action/"+"movebodypart"+"/"+"movebodypart"+"_0001.png","assets/action/"+"movebodypart"+"/"+"movebodypart"+"_0006.png");
+     }
+     if(this.action.name === "thinkabout" )
+     {
+     	this.animation = loadAnimation("assets/action/"+"thinkabout"+"/"+"thinkabout"+"_0001.png","assets/action/"+"thinkabout"+"/"+"thinkabout"+"_0003.png");
+     }
+     if(this.action.name === "hear" )
+     {
+     	this.animation = loadAnimation("assets/action/"+"hear"+"/"+"hear"+"_0001.png","assets/action/"+"hear"+"/"+"hear"+"_0002.png");
+     }
+     if(this.action.name === "feel" )
+     {
+     	this.animation = loadAnimation("assets/action/"+"feel"+"/"+"feel"+"_0001.png","assets/action/"+"feel"+"/"+"feel"+"_0003.png");
+     }
+	this.animation = loadAnimation("assets/action/"+this.action.name+"/"+this.action.name+"_0001.png","assets/action/"+this.action.name+"/"+this.action.name+"_0005.png");
 	this.panel = createSprite(i*250,100,20,20);
 	this.panel.addAnimation(this.action.name,this.animation);
 
